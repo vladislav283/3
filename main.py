3d8d7bb221a73ede4d3c42e8938f3fd4f8419881
vladislav283 <vlad9tarasov906@gmail.com>
2024-07-07 19:28:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,23 @@ while game:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
+        if sprite.collide_rect(racket, ball) or sprite.collide_rect(racket1, ball):
+            speed_x *= -1
+            speed_y *= 1
 
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over =True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+        
         racket.reset()
         racket1.reset()
         ball.reset()
